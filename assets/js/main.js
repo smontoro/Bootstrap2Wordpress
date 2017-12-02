d49e649e9f86d40b1c594b8aa0a0e36da5129a07
smontoro <sarahjmontoro@gmail.com>
2017-12-01 17:59:29 -0800

    added paralax effect to header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,26 @@
 $(function() {
-	
-	// Cache the Window object
+
+	//cche the window object
 	var $window = $(window);
-	
-	// Parallax Backgrounds
-	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
-	
-	$('section[data-type="background"]').each(function(){
-		var $bgobj = $(this); // assigning the object
-		
+
+	//Parallax background effect
+	$('section[data-type="background"]').each(function() {
+
+		var $bgobj = $(this); //assigning the object
+
 		$(window).scroll(function() {
-		
-			// Scroll the background at var speed
-			// the yPos is a negative value because we're scrolling it UP!								
+			//scroll the background at var speed
+			//the yPos is a negative valu because we're scrolling it up!
+
 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-			
-			// Put together our final background position
-			var coords = '50% '+ yPos + 'px';
-			
-			// Move the background
-			$bgobj.css({ backgroundPosition: coords });
-			
-		}); // end window scroll
+
+			//Put together our final background position
+
+			var coords = '50% ' + yPos + 'px';
+
+			//move the background
+			$bgobj.css({backgroundPosition: coords });
+
+		});
 	});
-	
 });
